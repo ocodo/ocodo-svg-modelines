c5b6b53abc6a29b99a6ad3e8dad7ebf80e571ca6
Jason Milkins <jason@opsmanager.com>
2015-05-09 23:04:00 +0800

    regenerate readme

--- ocodo-svg-modelines.el
+++ ocodo-svg-modelines.el
@@ -6,16 +6,21 @@
 ;;; Version: 0.1.0
 ;;
 ;;; Commentary:
-;;  A completely superfluous, but otherwise most excellent and awesome
-;;  modelines... if not now, then at some point in the future, you will
-;;  both want, love and need these.
+;;
+;;  # Ocodo SVG modelines
+;;
+;;  A completely superfluous, but otherwise most excellent collection
+;;  of awesome modelines... if not now, then at some point in the
+;;  future, you might want, need and maybe even love these.
+;;
+;;  <sub>Don't worry if this isn't true, it's just promotional bullshit.</sub>
 ;;
 ;;  Very much a work in progress, these modelines are relatively
 ;;  sparse, however, any info or widgets you'd like to have, please
 ;;  post an issue on the github repository
-;;  https://github.com/ocodo/ocodo-svg-modelines/issues and we'll
+;;  https://github.com/ocodo/ocodo-svg-modelines/issues and I'll
 ;;  attempt to cater to you.
-;;
+
 ;;; Code:
 
 (require 'svg-mode-line-themes)
