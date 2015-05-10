5752ea931f427a2a1ab8427d310f85cfef6ea5a1
Jason Milkins <jason@opsmanager.com>
2015-05-10 10:10:31 +0800

    Fix Kawaii active state

--- ocodo-kawaii-light-smt.el
+++ ocodo-kawaii-light-smt.el
@@ -54,25 +54,25 @@
   (list :font-weight "normal"
         :font-size "11pt"
         :font-family "sans-serif"
-        :fill (if (smt/window-active-p) "#000000" "#333333")))
+        :fill (if (smt/window-active-p) "#000000" "#777777")))
 
 (defun smt/ocodo-kawaii-light-major-mode-style (widget)
   (list :font-weight "normal"
         :font-size "11pt"
         :font-family "sans-serif"
-        :fill (if (smt/window-active-p) "#000000" "#333333")))
+        :fill (if (smt/window-active-p) "#000000" "#777777")))
 
 (defun smt/ocodo-kawaii-light-info-style (widget)
   (list :font-weight "normal"
         :font-size "6pt"
         :font-family "sans-serif"
-        :fill (if (smt/window-active-p) "#000000" "#333333")))
+        :fill (if (smt/window-active-p) "#000000" "#777777")))
 
 (defun smt/ocodo-kawaii-light-position-info-style (widget)
   (list :font-weight "normal"
         :font-size "8pt"
         :font-family "sans-serif"
-        :fill (if (smt/window-active-p) "#000000" "#333333")))
+        :fill (if (smt/window-active-p) "#000000" "#777777")))
 
 (defun smt/ocodo-kawaii-light-dirty-style (widget)
   (list :font-weight "normal"
@@ -87,13 +87,13 @@
 (defun smt/ocodo-kawaii-light-minor-mode-style (widget)
   (list :font-weight "normal"
         :font-size "6pt"
-        :fill (if (smt/window-active-p) "#000000" "#333333")))
+        :fill (if (smt/window-active-p) "#000000" "#777777")))
 
 (defun smt/ocodo-kawaii-light-version-control-style (widget)
   (list :font-weight "bold"
         :font-size "8pt"
         :font-family "sans-serif"
-        :fill (if (smt/window-active-p) "#5D3D70" "#333333")))
+        :fill (if (smt/window-active-p) "#5D3D70" "#777777")))
 
 (smt/deftheme ocodo-kawaii-light:smt
   :pixel-height 24
