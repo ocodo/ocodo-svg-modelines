761f5d29a990d199277fc249efd3412ac3fc5418
JasonM23 <jasonm23@users.noreply.github.com>
2015-05-11 00:19:19 +0800

    Update ocodo-svg-modelines.el

--- ocodo-svg-modelines.el
+++ ocodo-svg-modelines.el
@@ -103,6 +103,7 @@
 ;;
 ;; Subsequently, from Emacs:
 ;;
+;;     M-x package-install <RET> svg-mode-line-themes
 ;;     M-x package-install-file <RET> ~/ocodo-svg-modelines-0.1.0.tar
 ;;
 ;; ## Usage
